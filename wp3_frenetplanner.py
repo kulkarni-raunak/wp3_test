e2cbd02c1fad041e962fdc95159d703e97389c4b
Akhil Kanduri <73551215+akhilkanduri@users.noreply.github.com>
2021-01-30 17:04:52 +0100

    Create wp3_frenetplanner.py

---
+++ wp3_frenetplanner.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 30 16:57:15 2021
+
+@author: loki
+"""
+
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patch
+
+# Define
+
+def main():
+    #make a list of conditions
+    conditions = {
+            's0' : 0
+            
+            
+            
+            }
+if __name__ == '__main__' :
+    main()
