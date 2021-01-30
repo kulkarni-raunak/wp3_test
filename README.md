05de73546a200f563bf8a072789dbfdf769a7a4a
Akhil Kanduri <73551215+akhilkanduri@users.noreply.github.com>
2021-01-30 21:13:30 +0100

    Added New Updates

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # wp3_test
 Testing docker 
+
+#we have
+/obstacle
+/obstacle_waypoint
+/lane_waypoints_array
+/detected_polygons
+/detection/fusion_tools/objects
+/detection/fusion_tools/objects_markers
+/detection/image_detector/objects
+/detection/image_tracker/objects
+/detection/l_shaped/objects
+/detection/l_shaped/objects_markers
+/detection/lidar_detector/cloud_clusters
+/detection/lidar_detector/objects
+/detection/lidar_detector/objects_filtered
+/detection/lidar_detector/objects_markers
+/detection/lidar_tracker/objects
+/detection/lidar_tracker/objects_markers
+/detection/objects
+/detection_range
+rqt
--- wp3_Obstacle_get.py
+++ wp3_Obstacle_get.py
@@ -10,40 +10,47 @@ import time
 import rospy
 import tf
 import numpy as np
+import rospy
+import tf
+from nav_msgs.msg import Odometry
+from derived_object_msgs.msg import ObjectArray
+import gjk
+from autoware_msgs.msg import CloudClusterArray
+#
 
-from autoware_msgs.msg import Lane
-from autoware_msgs.msg import 
+def get_rect(x, y, yaw, vehicleSize):
 
-def get_obstacles(msg):
-	global Obstacles	
-	received_obstacles = msg
-	Obs_1=np.array([])
-	
-	f=open("wp3_obstacles_autoware.txt", "a+")
-	f.write('Obstacle Coordinates: ' + str(len(received_obstacles)) + '\n')
-	for i in range(len(received_waypoints.waypoints)):
-		f.write(str(received_waypoints.waypoints[i].pose.pose.position) + '\n')
-		path_wp_x = np.append(path_wp_x,received_waypoints.waypoints[i].pose.pose.position.x)
-	print('waypoints received') 
-	
+    dy = np.tan(yaw )
+    
+    headings = np.array([[1,dy]])
+    headings = headings/(np.sqrt(1+dy**2))
+  
+    normVec = np.full_like(headings,1)
+    normVec[0,0] = -1*headings[0,1]
+    normVec[0,1] = headings[0,0]
 
-if __name__ == '__main__':
-	try:
-				
-		global path_wp_x,path_wp_y,path_wp_z
-		path_wp_x = np.array([])
-		path_wp_y = np.array([])
-		path_wp_z = np.array([])
-		rospy.init_node('wp3_waypoints', anonymous=True)
-		waypoints_sub = rospy.Subscriber('/final_waypoints', Lane,waypoints_callback)
-		while not rospy.is_shutdown(): #rate can be modified when needed
-			if (len(path_wp_x) and len(path_wp_y) and len(path_wp_z))> 0 : 
-				print('Entered if loop'+'\n')
-				print('x: ' + str(path_wp_x) + '\n')
-				print('y: ' + str(path_wp_y) + '\n')
-				print('z: ' + str(path_wp_z) + '\n')
-		rospy.spin()
-			
-	except rospy.ROSInterruptException:
-		
-		pass
+    locn = np.array([[x, y]])
+        
+    topleft = locn + normVec[:,0:2] * 0.5* vehicleSize[1] - headings[:,0:2] * 0.5 * vehicleSize[0] 
+    bottomleft = locn - normVec[:,0:2] * 0.5* vehicleSize[1] - headings[:,0:2] * 0.5 * vehicleSize[0] 
+    topright = topleft + headings[:,0:2] * vehicleSize[0]
+    bottomright = bottomleft + headings[:,0:2] * vehicleSize[0]
+
+    return [topleft, bottomleft, bottomright, topright]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
