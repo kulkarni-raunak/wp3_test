9734d6653369280e69fbcf3364e17f4d5067cf09
Akhil Kanduri <73551215+akhilkanduri@users.noreply.github.com>
2021-02-14 17:11:28 +0100

    invoke launch from .py

---
+++ Launch_WP3.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 14 16:49:25 2021
+
+@author: lfzt3
+"""
+
+import roslaunch
+import rospy
+
+rospy.init_node('WP3_Autoware', anonymous=True)
+uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+roslaunch.configure_logging(uuid)
+launch = roslaunch.parent.ROSLaunchParent(uuid, ["/home/carla-autoware/carla_autoware_agent/launch/carla_autoware_agent.launch"])
+launch.start()
+rospy.loginfo("started")
+
+rospy.sleep(3)
+# 3 seconds later
+# launch.shutdown()
