c8fdc5c23e410315e8d30537aa721761a0c77f91
Akhil Kanduri <73551215+akhilkanduri@users.noreply.github.com>
2021-02-14 17:38:52 +0100

    Update Trail_Launch.py

--- Trail_Launch.py
+++ Trail_Launch.py
@@ -8,13 +8,24 @@ Created on Sun Feb 14 16:49:25 2021
 import roslaunch
 import rospy
 
-rospy.init_node('WP3_Autoware', anonymous=True)
-uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-roslaunch.configure_logging(uuid)
-launch = roslaunch.parent.ROSLaunchParent(uuid, ["/home/autoware/carla-autoware/carla-autoware-agent/launch/carla_autoware_agent.launch"])
-launch.start()
-rospy.loginfo("started")
-
-rospy.sleep(3)
+#rospy.init_node('WP3_Autoware', anonymous=True)
+#uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+#roslaunch.configure_logging(uuid)
+#launch = roslaunch.parent.ROSLaunchParent(uuid, ["/home/autoware/carla-autoware/carla-autoware-agent/launch/carla_autoware_agent.launch"])
+#launch.start()
+#rospy.loginfo("started")
+#rospy.sleep(3)
 # 3 seconds later
 # launch.shutdown()
+
+
+package = 'carla-autoware-agent'
+executable = 'carla-autoware-agent'
+node = roslaunch.core.Node(package,executable)
+launch = roslaunch.scriptapi.ROSLaunch()
+launch.start()
+
+process = launch.launch(node)
+print(process.is_alive())
+
+
